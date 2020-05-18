8ce6ef054b21fbfcd989331c33047f3224e752ea
Min <miny7050@gmail.com>
2020-05-18 10:57:41 +0900

    first commit

---
+++ app/src/main/java/com/beeori/androidxsample/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.beeori.androidxsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        test_id.text = "This is Kotlin Android Extensions test project!"
+    }
+}
